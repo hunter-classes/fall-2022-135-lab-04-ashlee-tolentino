0ed7abde4323d9a04cdc9f8d965b6fe49c33c922
Ashlee Tolentino <ashleetolentino@Ashlees-Air.lan>
2022-09-21 14:07:21 -0400

    checkerboard function done

--- main.cpp
+++ main.cpp
@@ -18,6 +18,11 @@ int main()
   std::cout << "box(7,4):\n";
   std::cout << result;
   std::cout << "\n------------------\n"; // separator
+
+  std::string result2 = checkerboard(11,6);
+  std::cout << "checkerboard(11,6):\n";
+  std::cout << result2;
+  std::cout << "\n------------------\n"; // separator
   
   return 0;
 }
