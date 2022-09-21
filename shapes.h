63f1474f530d5c908695120320614a707c9f2d9d
Ashlee Tolentino <ashleetolentino@Ashlees-Air.lan>
2022-09-21 13:48:20 -0400

    .h file done

---
+++ shapes.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+#pragma once
+
+std::string box(int, int);
+std::string checkerboard(int, int);
+std::string cross(int);
+std::string lower(int);
+std::string upper(int);
+std::string trapezoid(int, int);
+std::string checkerboard3x3(int, int);
